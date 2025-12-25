765b712a7ec8cd0127842d1affc3286f3ff7fd3d
backguynn <backguyn@netlox.io>
2025-12-25 16:17:29 +0900

    what

--- cicd/tcplb-local/config.sh
+++ cicd/tcplb-local/config.sh
@@ -6,7 +6,7 @@ echo "#########################################"
 echo "Spawning all hosts"
 echo "#########################################"
 
-spawn_docker_host --dock-type loxilb --dock-name llb1
+spawn_docker_host --dock-type loxilb --dock-name llb1 --extra-args "--localsockpolicy"
 
 echo "#########################################"
 echo "Connecting and configuring  hosts"
